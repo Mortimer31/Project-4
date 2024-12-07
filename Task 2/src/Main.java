34467b831564fed81d940bc046c9e87a9598fde9
Mortimer31 <samaritilus@gmail.com>
2024-12-07 23:55:54 +0300

    Task complete

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 2 == 0)
+//                System.out.println("Уволен сотрудник c id" + i);
+//            }
+
+        int i = 2;
+        while (i<=100) {
+                i += 2;
+                System.out.println("Уволен сотрудник с id" + i);
+
+        }
+
     }
 }
